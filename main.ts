3403b65bc17e30cd8234f98803efc0e059da9ba2
easonlawtm <115674665+easonlawtm@users.noreply.github.com>
2022-10-20 09:43:29 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (c))
+    c += 1
 })
+input.onButtonPressed(Button.AB, function () {
+    while (c >= 1) {
+        basic.showString("" + (c))
+        c += -1
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("" + (c))
+    c += 10
+})
+let c = 0
+c = 0
